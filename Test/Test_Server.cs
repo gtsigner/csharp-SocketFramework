6f913c52f2d8932068c2bf1eeb16a5771ddc60fb
zhaojunlike <zhaojunlike@gmail.com>
2016-12-20 21:46:06 +0800

    test_server

---
+++ Test/Test_Server.cs
@@ -0,0 +1,25 @@
+﻿using OeynetSocket.SocketFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+
+namespace OeynetSocket.Test
+{
+    class Test_Server
+    {
+        public static void Main(String[] args)
+        {
+            IPAddress addr = IPAddress.Parse("127.0.0.1");
+            SocketServer server = new SocketServer(addr, 6666);
+            server.OnClientConnected += server_OnClientConnected;
+            server.StartListen();
+            Console.Read();
+        }
+        static void server_OnClientConnected(ConnectEventType type, SocketEventArgs args)
+        {
+            Console.WriteLine("One Client Connected: ip" + args.RemoteAddress);
+        }
+    }
+}
